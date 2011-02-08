335ad3f4d862b616e8ec25610a57246784372f7e
brian.dunlay <brian.dunlay@gmail.com>
2011-02-08 03:59:58 +0000

    initial upload

---
+++ src/com/ece194/globetrotter/GlobeTrotter.java
@@ -0,0 +1,13 @@
+package com.ece194.globetrotter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GlobeTrotter extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
