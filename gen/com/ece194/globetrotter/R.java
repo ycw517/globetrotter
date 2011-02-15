3017fcd79f1f9f371be1689cfbcf817056cb1418
brian.dunlay <brian.dunlay@gmail.com>
2011-02-15 07:01:59 +0000

    created buttons to launch /add tag/ and /view tags/ activities

--- gen/com/ece194/globetrotter/R.java
+++ gen/com/ece194/globetrotter/R.java
@@ -12,17 +12,22 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int tag_button=0x7f020001;
+        public static final int tag_normal=0x7f020002;
+        public static final int view_button=0x7f020003;
+        public static final int view_normal=0x7f020004;
     }
     public static final class id {
-        public static final int Button01=0x7f050000;
-        public static final int Button03=0x7f050001;
-        public static final int Spinner01=0x7f050002;
+        public static final int tag_button=0x7f050000;
+        public static final int view_button=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int tag=0x7f040002;
+        public static final int title=0x7f040000;
+        public static final int view=0x7f040003;
     }
 }
