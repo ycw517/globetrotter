fe8942fa5314552b692d42fbc46efe5124f43feb
brian.dunlay@gmail.com <brian.dunlay@gmail.com@3e93ef72-38d5-a9fe-0505-d210f8d4a803>
2011-02-15 07:15:33 +0000

    added toast

--- trunk/src/com/ece194/globetrotter/GlobeTrotter.java
+++ trunk/src/com/ece194/globetrotter/GlobeTrotter.java
@@ -1,7 +1,10 @@
 package com.ece194.globetrotter;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class GlobeTrotter extends Activity {
     /** Called when the activity is first created. */
@@ -10,4 +13,16 @@ public class GlobeTrotter extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    
+
+    
+	public void makeToast(View view) {
+		
+		Context context = getApplicationContext();
+		int duration = Toast.LENGTH_SHORT;
+		String text = "Don't touch me!";
+		Toast.makeText(context, text, duration).show();
+	}
+    
+    
 }
