5bcdb9629e42a330ae20ca35192c324bbf2c4c93
brian.dunlay <brian.dunlay@gmail.com>
2011-02-16 04:34:20 +0000

    Added a camera preview activity

--- gen/com/ece194/globetrotter/R.java
+++ gen/com/ece194/globetrotter/R.java
@@ -18,11 +18,14 @@ public final class R {
         public static final int view_normal=0x7f020004;
     }
     public static final class id {
-        public static final int tag_button=0x7f050000;
-        public static final int view_button=0x7f050001;
+        public static final int layout=0x7f050000;
+        public static final int preview=0x7f050001;
+        public static final int tag_button=0x7f050002;
+        public static final int view_button=0x7f050003;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int capture=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
