3759dcac4c5c46c919cd6b48850ddbaa2c8949c8
brian.dunlay@gmail.com <brian.dunlay@gmail.com@3e93ef72-38d5-a9fe-0505-d210f8d4a803>
2011-02-16 21:42:35 +0000

    really added the viewer this time

---
+++ trunk/src/com/ece194/globetrotter/ViewerActivity.java
@@ -0,0 +1,19 @@
+package com.ece194.globetrotter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ViewerActivity extends Activity {
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.viewer);
+        
+  //      ImageView image = (ImageView) findViewById(R.id.viewer);
+  //      image.setImageResource();
+
+        
+        
+    }
+}
