8c27d564ab1019734e6203178101e311b0d73010
brian.dunlay <brian.dunlay@gmail.com>
2011-02-16 04:34:58 +0000

    adding cameraactivity.java

---
+++ src/com/ece194/globetrotter/CameraActivity.java
@@ -0,0 +1,89 @@
+package com.ece194.globetrotter;
+import android.app.Activity;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+public class CameraActivity extends Activity {
+	private SurfaceView preview=null;
+	private SurfaceHolder previewHolder=null;
+	private Camera camera=null;
+	private boolean inPreview=false;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.capture);
+
+		
+
+
+		
+		preview=(SurfaceView)findViewById(R.id.preview);
+		
+		
+
+		previewHolder=preview.getHolder();
+		previewHolder.addCallback(surfaceCallback);
+		previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		camera=Camera.open();
+	}
+
+	@Override
+	public void onPause() {
+		if (inPreview) {
+			camera.stopPreview();
+		}
+
+		camera.release();
+		camera=null;
+		inPreview=false;
+
+		super.onPause();
+	}
+
+	SurfaceHolder.Callback surfaceCallback=new SurfaceHolder.Callback() {
+		public void surfaceCreated(SurfaceHolder holder) {
+			try {
+				camera.setPreviewDisplay(previewHolder);
+			}
+			catch (Throwable t) {
+				Log.e("PreviewDemo-surfaceCallback",
+							"Exception in setPreviewDisplay()", t);
+				Toast
+					.makeText(CameraActivity.this, t.getMessage(), Toast.LENGTH_LONG)
+					.show();
+			}
+		}
+
+		public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+			Camera.Parameters p=camera.getParameters();
+
+			p.set("rotation", 180); 
+			previewHolder.getSurface(); 
+			Surface.setOrientation(Display.DEFAULT_DISPLAY, 
+			Surface.ROTATION_90); 
+
+			
+	//		p.setPreviewSize(width, height);
+			camera.setParameters(p);
+			camera.startPreview();
+			inPreview=true;
+		}
+
+		public void surfaceDestroyed(SurfaceHolder holder) {
+			// no-op
+		}
+	};
+}
