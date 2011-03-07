93e7baf57e471d02b1171dd186d4226beab593c3
brian.dunlay@gmail.com <brian.dunlay@gmail.com@3e93ef72-38d5-a9fe-0505-d210f8d4a803>
2011-03-07 12:57:48 +0000

    added async task to query server and display a progress bar while the photo stitches

--- trunk/src/com/ece194/globetrotter/CameraActivity.java
+++ trunk/src/com/ece194/globetrotter/CameraActivity.java
@@ -2,7 +2,10 @@
 package com.ece194.globetrotter;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.http.HttpEntity;
@@ -11,25 +14,34 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.hardware.Camera;
 import android.hardware.Camera.PreviewCallback;
 import android.media.MediaRecorder;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
 
-public class CameraActivity extends Activity implements SurfaceHolder.Callback, Camera.AutoFocusCallback {
+public class CameraActivity<ExampleApp> extends Activity implements SurfaceHolder.Callback, Camera.AutoFocusCallback {
 
+	public String project_name = "globetrotter-test-05";
+	
+	
 	private SurfaceView preview;
 	private SurfaceHolder previewHolder;
 	
@@ -40,33 +52,53 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback, 
 	private int frame_number = 0;
 	private byte[] frame = new byte[1];
 	
+	private byte[] progressBuffer = new byte[4];
+	
+	private TextView mPreviewText;
+	
+	ProgressDialog  pDialogue;
+	
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.capture);
 
+		setContentView(R.layout.capture);
 	    preview=(SurfaceView)findViewById(R.id.preview);
 	    previewHolder=preview.getHolder();
 		previewHolder.addCallback(this);
 	    previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 	    
-	    
-	    
-	//    mRecorder = new MediaRecorder();
+	    mPreviewText=(TextView)findViewById(R.id.previewText);
+	    mPreviewText.setText("Frames Captured: " + frame_number);
+	    //getDateTime() //for timestamping the project 
+	    pDialogue = new ProgressDialog( CameraActivity.this );
+	    pDialogue.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+	    pDialogue.setMessage("Loading...");
+	    pDialogue.setCancelable(false);
+    	pDialogue.setProgress(0);
 
-	}
+
+
+	   	}
 
 	@Override
 	public void onResume() {
-		super.onResume();
+		super.onResume();		
 	}
 
 	@Override
 	public void onPause() {
-		super.onPause();
+  		super.onPause();
+  		
+		if (mPreviewRunning) {
+			mCamera.stopPreview();
+			mPreviewRunning = false;
+		}
+		mCamera.setPreviewCallback(null); // this is necessary to prevent the callback from trying to access an empty surfaceview
+		 mCamera.release();		
 	}
 	
-
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -77,11 +109,18 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback, 
 
 	public void startRecording() {
 		mCaptureFrame = true;
-		
-	//	mRecorder.start();
 	}
 	
+	public void captureFrame(View v) {
+		mCaptureFrame = true;
+
+	}
+
+	
+	
 	public void stopRecording() {
+		
+		
 	//	mRecorder.stop();
         HttpClient httpclient = new DefaultHttpClient();  
         HttpPost httppost = new HttpPost("http://dragonox.cs.ucsb.edu/Mosaic3D/process.php");
@@ -89,7 +128,7 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback, 
         try {  
             // Add your data  
             List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(6);  
-            nameValuePairs.add(new BasicNameValuePair("project", "globetrotter-test-02"));  
+            nameValuePairs.add(new BasicNameValuePair("project", project_name));  
             nameValuePairs.add(new BasicNameValuePair("width", "480"));  
             nameValuePairs.add(new BasicNameValuePair("height", "320"));  
             nameValuePairs.add(new BasicNameValuePair("eFocal", "36"));  
@@ -138,14 +177,11 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback, 
 
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
-		 mCamera.stopPreview();
-		 mPreviewRunning = false;
-		 mCamera.release();		
-		 
-	//	 mRecorder.reset();
-	//	 mRecorder.release();
+		mPreviewRunning = false;
 
 	}
+	
+
 
 	@Override
 	public void onAutoFocus(boolean success, Camera camera) {
@@ -165,56 +201,119 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback, 
 	*/
 	PreviewCallback previewCallback = new PreviewCallback () {
 		public void onPreviewFrame(byte[] data, Camera camera) {
-			
 			if (mCaptureFrame) {
 				mCaptureFrame = false;
 				frame[0] = (byte)frame_number;
 				new FrameHandler().execute(data, frame);
 				frame_number++;
+				
+			    mPreviewText.setText("Frames Captured: " + frame_number);
 			}
-			
-			
 		}
 	};
 	
 	
 	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-		/*
-		mRecorder.reset();
-	    mRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
-	    mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-	    mRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.MPEG_4_SP);
-	    mRecorder.setOutputFile("/sdcard/globetrotter/video.mp4");
-	    mRecorder.setVideoFrameRate(30);
-	    
-		mRecorder.setPreviewDisplay(previewHolder.getSurface());
-	    try {
-			mRecorder.prepare();
-		} catch (IllegalStateException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+	
+		if (mPreviewRunning) {
+			mCamera.stopPreview();
 		}
-
-	*/	
-		if (mPreviewRunning) mCamera.stopPreview();
+		
 
 		Camera.Parameters p = mCamera.getParameters();
+
 		p.setPreviewSize(width, height);
+		p.setPreviewSize(480, 320);
 		mCamera.setParameters(p);
 
 		try { mCamera.setPreviewDisplay(holder); }
 		catch (IOException e) { e.printStackTrace(); }	
 
 		mCamera.setPreviewCallback(previewCallback);
-		
-		
+
+		// 0 = landscape
+		// 90 = portrait
+
+		    int rotation = getWindowManager().getDefaultDisplay().getRotation();
+		    int degrees = 0;
+		    switch (rotation) {
+		        case Surface.ROTATION_0: degrees = 90; break;
+		        case Surface.ROTATION_90: degrees = 0; break;
+		        case Surface.ROTATION_180: degrees = 270; break;
+		        case Surface.ROTATION_270: degrees = 180; break;
+		        
+		    }
+	        Log.i("DEGREES ARE WHAT??", Integer.toString(degrees));
+
+		mCamera.setDisplayOrientation(degrees);		
+
 		
 		mCamera.startPreview();
 		mPreviewRunning = true;
 		
 	}
 	
+    private String getDateTime() {
+        DateFormat dateFormat = new SimpleDateFormat("H,H,m,m,s,s");
+        Date date = new Date();
+        return dateFormat.format(date);
+    }
+    
+    public void toast(View v){
+    	stopRecording();
+    	
+		mPreviewRunning = false;
+		mCamera.stopPreview();
+
+		pDialogue.show();
+
+		new DownloadFilesTask().execute();
+    }
+    
+    private class DownloadFilesTask extends AsyncTask<Void, Integer, Void> {
+    	
+
+        protected Void doInBackground(Void... urls) {
+        	int response = 0;
+        	// should clean this up so it doesn't create so many objects
+        	while (response != 100) {
+	        	try {
+	                HttpClient client = new DefaultHttpClient();  
+	                String getURL = "http://dragonox.cs.ucsb.edu/Mosaic3D/uploads/"+ project_name +"/status.txt";
+	                HttpGet get = new HttpGet(getURL);
+	                HttpResponse responseGet = client.execute(get);  
+	                HttpEntity resEntityGet = responseGet.getEntity();  
+	                if (resEntityGet != null) {  
+	                    //do something with the response
+	                	response = Integer.valueOf(EntityUtils.toString(resEntityGet).trim());
+	                    Log.i("GET RESPONSE","RESPONSE IS = " + response);
+	                    publishProgress(response);
+	                }
+		        } catch (Exception e) {
+		            Log.i("Y U NO WORK?",e.toString());
+	
+		        }
+        	}
+        	        	
+			return null;
+        }
+
+        protected void onProgressUpdate(Integer... progress) {     
+            Log.i("UPDATE","UPDATE = ");
+ 
+        	pDialogue.setProgress(progress[0]);
+
+        }
+
+        protected void onPostExecute(Void blah) {
+            Log.i("COMPLETE","COMPLETE");
+
+        	pDialogue.dismiss();
+        	finish(); // this could also start an intent to another activity, like a list, and then finish here
+        }
+    }
+
+    
 
 }
