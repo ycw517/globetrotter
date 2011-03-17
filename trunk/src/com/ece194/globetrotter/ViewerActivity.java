c4c444898e92ac989faa5a900599e3fe516f011d
brian.dunlay@gmail.com <brian.dunlay@gmail.com@3e93ef72-38d5-a9fe-0505-d210f8d4a803>
2011-03-17 10:51:07 +0000

    added icons

--- trunk/src/com/ece194/globetrotter/ViewerActivity.java
+++ trunk/src/com/ece194/globetrotter/ViewerActivity.java
@@ -3,14 +3,13 @@ package com.ece194.globetrotter;
 import android.app.Activity;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import com.ece194.pan.ImageViewer;
 import com.ece194.pan.PanCompassListener;
